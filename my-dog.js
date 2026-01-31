d7e57412c425cea307316b7b0b4bc5b7b6a666ed
ashtalfa794-bit <ashtalfa794@gmail.com>
2026-01-31 18:11:48 +0000

    dog completed

---
+++ my-dog.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
+
+function draw(){
+  background(220)
+  //background(red,green,blue)
+  fill("brown");
+  circle(-28,-28,35);
+  
+  
+  fill("orange");
+  rect(-14,-28,72,55);
+  
+  fill("white");
+  rect(-14,26,9,44);
+  
+   fill("white");
+  rect(-1,26,9,44);
+  
+     fill("white");
+  rect(17,26,9,44);
+  
+       fill("white");
+  rect(30,26,9,44);
+  
+  fill("blue");
+  ellipse(55,-35,11,45);
+  
+    fill("white");
+  ellipse(-49,-30,11,41);
+  
+      fill("white");
+  ellipse(-10,-30,11,41);
+  
+  fill("orange");
+  circle(-24,-34,10);
+  
+    fill("orange");
+  circle(-33,-34,10);
+  
+      fill("orange");
+  circle(-27,-20,10);
+  
+  fill("green");
+  rect(200,200,-500,-128);
+  
+    fill("yellow");
+  circle(-170,-170,55);
+}
